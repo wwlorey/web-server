eb83f30a71a104e45f87a8f73c51033aca47a5b0
William Lorey <wwlorey@gmail.com>
2018-09-11 20:56:56 -0500

    Fix client & config variables

--- WebServer.py
+++ WebServer.py
@@ -14,7 +14,7 @@ serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Prepare a sever socket
 serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-serverSocket.bind((config['serverName'], int(config['port'])))
+serverSocket.bind((config['defaultServerAddr'], int(config['defaultPort'])))
 
 serverSocket.listen(2)
 
--- client.py
+++ client.py
@@ -16,14 +16,13 @@ if len(sys.argv) == int(config['numExpectedArgs']):
     serverAddr = sys.argv[int(config['serverAddrArgIndex'])]
     port = int(sys.argv[int(config['portArgIndex'])])
     fileName = sys.argv[int(config['fileNameArgIndex'])]
+
 else:
     # Default to the values in config
-    serverAddr = config['defaultServerName']
+    serverAddr = config['defaultServerAddr']
     port = int(config['defaultPort'])
     fileName = config['defaultFileName']
 
-
-exit_client = False
 
 # Create the client socket
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -32,39 +31,29 @@ clientSocket.settimeout(float(config['recvDelay']))
 # Establish the connection
 clientSocket.connect((serverAddr, port))
 
-while True:
-    # Get the file name from the user
-    file_name = input('Enter file name ("exit" to exit): ')
-    if file_name == 'exit':
-        break
+# Request the file from the server
+get_request = 'GET /' + str(fileName) + ' HTTP/1.1\r\n'
+clientSocket.send(get_request.encode())
+clientSocket.send('\r\n'.encode())
 
-    # Request the file from the server
-    get_request = 'GET /' + str(file_name) + ' HTTP/1.1\r\n'
-    clientSocket.send(get_request.encode())
-    clientSocket.send('\r\n'.encode())
+# Ensure all messages are received
+target_time = time.time() + float(config['recvDelay'])
+while target_time > time.time():
+    try:
+        # Receive and decode the message
+        file_contents = clientSocket.recv(2048).decode()
 
-    # Ensure all messages are received
-    target_time = time.time() + float(config['recvDelay'])
-    while target_time > time.time():
-        try:
-            # Receive and decode the message
-            file_contents = clientSocket.recv(2048).decode()
+        # Only print the message if it is not empty
+        if file_contents:
+            print(file_contents)
 
-            # Only print the message if it is not empty
-            if file_contents:
-                print(file_contents)
-
-            if '404 Not Found' in file_contents:
-                # The file was not found by the server
-                exit_client = True
-                break
-
-        except:
-            # Handle the client socket receive timeout
-            pass
-    
-        if exit_client:
+        if '404 Not Found' in file_contents:
+            # The file was not found by the server
             break
+
+    except:
+        # Handle the client socket receive timeout
+        pass
 
 # Close the socket and exit the program
 print('Client exiting.')
