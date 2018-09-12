34fbf6136daa60c3a5cb27c91119f652173c8a29
William Lorey <wwlorey@gmail.com>
2018-09-11 21:39:25 -0500

    Implement webserver threading

--- WebServer.py
+++ WebServer.py
@@ -3,7 +3,50 @@
 
 import configparser
 import socket
-import sys
+import threading
+
+
+def connectionHandler(clientSocket, addr):
+    while True:
+        try:
+            # Receive a message from the client
+            message = clientSocket.recv(2048).decode()
+
+            if message != '\r\n':
+                try:
+                    # Assume we have a HTTP GET message
+                    filename = message.split()[1][1:]
+                except:
+                    # The client is sending unexpected messages. Break out of the loop
+                    break
+
+                print('File requested: ' + filename + ' ' + str(addr))
+
+                with open(filename, 'r') as f:
+                    outputdata = f.read()
+
+                # Send the HTTP header into the socket
+                clientSocket.send('HTTP/1.1 200 OK\r\n'.encode())
+                clientSocket.send('Content-Type: text/html\r\n'.encode())
+                clientSocket.send('Connection: keep-alive\r\n'.encode())
+                clientSocket.send(('Content-Length: ' + str(len(outputdata.encode('utf-8'))) + '\r\n').encode())
+                clientSocket.send('\r\n'.encode())
+
+                # Send the content of the requested file to the client
+                clientSocket.send((outputdata + '\f\r\n').encode())
+
+        except IOError:
+            # Send response message for file not found
+            clientSocket.send('HTTP/1.1 404 Not Found\r\n'.encode())
+            clientSocket.send('\r\n'.encode())
+
+            # Close client socket and break out of the loop
+            print('Could not serve file ' + str(addr))
+            break
+
+    print('Closing client socket ' + str(addr))
+    clientSocket.close()
+
 
 # Initialize configuration
 config = configparser.ConfigParser()
@@ -16,52 +59,21 @@ serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 serverSocket.bind((config['defaultServerAddr'], int(config['defaultPort'])))
 
-serverSocket.listen(2)
-
-# Establish the connection
-clientSocket, addr = serverSocket.accept()
+serverSocket.listen(int(config['backlog']))
 print('Ready to serve...')
 
+threads = []
+
 while True:
-    try:
-        # Receive a message from the client
-        message = clientSocket.recv(2048).decode()
+    # Establish the connection
+    clientSocket, addr = serverSocket.accept()
+    print('Got connection from ' + str(addr))
+    newThread = threading.Thread(target=connectionHandler, args=(clientSocket, addr))
+    newThread.start()
+    threads.append(newThread)
 
-        if message != '\r\n':
-            try:
-                # Assume we have a HTTP GET message
-                filename = message.split()[1][1:]
-            except:
-                # The client is sending unexpected messages. Break out of the loop
-                break
-
-            print('File requested: ' + filename)
-
-            with open(filename, 'r') as f:
-                outputdata = f.read()
-
-            # Send the HTTP header into the socket
-            clientSocket.send('HTTP/1.1 200 OK\r\n'.encode())
-            clientSocket.send('Content-Type: text/html\r\n'.encode())
-            clientSocket.send('Connection: keep-alive\r\n'.encode())
-            clientSocket.send(('Content-Length: ' + str(len(outputdata.encode('utf-8'))) + '\r\n').encode())
-            clientSocket.send('\r\n'.encode())
-
-            # Send the content of the requested file to the client
-            clientSocket.send((outputdata + '\f\r\n').encode())
-
-    except IOError:
-        # Send response message for file not found
-        clientSocket.send('HTTP/1.1 404 Not Found\r\n'.encode())
-        clientSocket.send('\r\n'.encode())
-
-        # Close client socket and break out of the loop
-        print('Could not serve file.')
-        print('Closing client socket.')
-        clientSocket.close()
-        break
-
-# Close the socket and exit the program
+# Join the threads and close the socket
 print('Server exiting.')
+for thread in threads:
+    thread.join()
 serverSocket.close()
-sys.exit()
